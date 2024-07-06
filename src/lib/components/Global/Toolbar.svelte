e2f6206a7c82535a16581337453633a5c91e21c0
Gagan Suie <gagan.suie@codecrow.io>
2024-07-06 02:20:20 -0500

    Fix: toolbar responsive changes

--- src/lib/components/Global/Toolbar.svelte
+++ src/lib/components/Global/Toolbar.svelte
@@ -1,6 +1,7 @@
 <script>
 	import IconDownChevron from '$lib/assets/icons/IconDownChevron.svelte'
 	import IconRightChevron from '$lib/assets/icons/IconRightChevron.svelte'
+	import IconSearch from '$lib/assets/icons/IconSearch.svelte'
 </script>
 
 <div class="navbar bg-base-100">
@@ -22,7 +23,17 @@
 			<ul
 				tabindex="0"
 				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-				<li><a href="/messages">Jobs <IconDownChevron /></a></li>
+				<li>
+					<details>
+						<summary class="custom-menu-item"> Jobs </summary>
+						<ul class="p-2 ml-5">
+							<li><a href="/job-post">Post a job</a></li>
+							<li><a href="/dashboard">Dashboard</a></li>
+							<li><a href="/previous-job-posts">Job post history</a></li>
+							<li><a href="/previous-contracts">Contract history</a></li>
+						</ul>
+					</details>
+				</li>
 				<li><a href="/messages">Messages</a></li>
 				<!-- <li><a href="/reports">Reports</a></li> -->
 			</ul>
@@ -56,16 +67,7 @@
 			<div class="form-control">
 				<label class="input input-bordered flex items-center gap-2">
 					<input type="text" placeholder="Search" class="w-24 md:w-auto" />
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 16 16"
-						fill="currentColor"
-						class="h-4 w-4 opacity-70">
-						<path
-							fill-rule="evenodd"
-							d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-							clip-rule="evenodd" />
-					</svg>
+					<IconSearch />
 				</label>
 			</div>
 			<div class="dropdown dropdown-end">
