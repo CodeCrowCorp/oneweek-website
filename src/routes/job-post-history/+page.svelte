1113423dd49bba729b51d51c99e1d06c43d8450a
Gagan Suie <gagan.suie@codecrow.io>
2024-07-10 12:44:13 -0500

    Feat: landing page, waitlist, and auth

--- src/routes/job-post-history/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores'
-</script>
-
-<div class="hero min-h-screen bg-base-200">Job Post History</div>
---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,23 @@
+import { redirect } from '@sveltejs/kit'
+import type { Actions, PageServerLoad } from './$types'
+
+export const load = (() => {
+	// redirect the user
+	redirect(302, '/dashboard')
+}) satisfies PageServerLoad
+
+export const actions: Actions = {
+	default({ cookies }) {
+		// eat the cookie
+		const cookieItem = ['token', 'userId', 'user']
+		cookieItem.forEach((item) => {
+			cookies.set(item, '', {
+				path: '/',
+				expires: new Date(0)
+			})
+		})
+
+		// redirect the user
+		redirect(302, '/landing')
+	}
+}
